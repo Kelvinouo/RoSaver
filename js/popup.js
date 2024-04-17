10cb32d03414ea682bed7c4d3695387438995123
Fern <915642604cc@gmail.com>
2024-04-17 20:27:50 +0800

    popup fix

--- js/popup.js
+++ js/popup.js
@@ -24,8 +24,22 @@ async function notification(title, message) {
         notification("Success changing placeid", "Refresh the tab to apply changes")
         window.location.reload()
     })
-    
+
     let a = await fetch("https://raw.githubusercontent.com/Kelvinouo/RoSaver/master/news.txt").then(d => d.text())
     console.log(a)
     $(".rsaver").append(a)
+
+    $("#simuna").on("click", () => {
+        chrome.tabs.create({
+            url: `https://discord.gg/frrQSPVajK`,
+            active: true
+        });
+    })
+
+    $("#discord").on("click", () => {
+        chrome.tabs.create({
+            url: `https://discord.gg/Bc2yG4Ea52`,
+            active: true
+        });
+    })
 })();
